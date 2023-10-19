fe5084d7944a702957ceaf9af115ed23a4baf746
MatheusZanon <mzblannes@outlook.com>
2023-10-19 11:52:32 -0300

    Adicionando modulo FS e instalando Express para lidar com http

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const { Person } = require("./person.js");
 
 //require("./modules/path.js");
-require("./modules/fs.js");
+//require("./modules/fs.js");
+//require("./modules/http.js");
+
+require("./modules/express.js");
 
 const person = new Person("Fulano de Tal");
 
